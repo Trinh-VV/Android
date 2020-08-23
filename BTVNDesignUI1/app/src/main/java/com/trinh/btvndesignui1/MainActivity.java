28d0720eb3f670bf5b1dc3432df6fd7d947aac46
trinh093 <69407546+trinh093@users.noreply.github.com>
2020-08-23 22:35:26 +0700

    commit
    23_8 update

---
+++ BTVNDesignUI1/app/src/main/java/com/trinh/btvndesignui1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.trinh.btvndesignui1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login_fb);
+    }
+}
